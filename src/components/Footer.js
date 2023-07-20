23ecca14f7de9bc5d56c3acecb7d7ddb770de367
Dave Pagaduan <dave.c.pagaduan@gmail.com>
2023-07-20 11:38:56 +1000

    added projects and revamped contact me

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 import './Footer.css';
 
 
@@ -7,7 +8,38 @@ function Footer() {
     <div className='footer-container'>
       <h1 id='contact-me'>Contact Me</h1>
       <section className='contact-links'>
+        <Card className='links'>
+          <Card.Body>
+            <Card.Title>
+              <i class="fa-solid fa-envelope fa-lg"></i> Email
+            </Card.Title>
+            <Card.Text>
+              Let's get in touch
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
+        <Card className='links'>
+          <Card.Body>
+            <Card.Title>
+              <i class="fa-brands fa-linkedin fa-lg"></i> LinkedIn
+            </Card.Title>
+            <Card.Text>
+              Let's connect
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card className='links'>
+          <Card.Body>
+            <Card.Title>
+              <i class="fa-brands fa-github fa-lg"></i> Github
+            </Card.Title>
+            <Card.Text>
+              Check out my repos
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </section>
     </div>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Projects.css'
+
+export default function Projects() {
+  return (
+    <div id="projects">
+      <h1 className='projects-title'>Projects</h1>
+    </div>
+  )
+}
